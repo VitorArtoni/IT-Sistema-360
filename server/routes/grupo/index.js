9574cb9bd0ecd697eb3bbf55f32c113fb3a2f19b
VitorArtoni <vitor_artonio@hotmail.com>
2019-05-17 19:04:11 -0300

    Atualizado documentação do Swagger para Grupo e Login

--- server/routes/grupo/index.js
+++ server/routes/grupo/index.js
@@ -11,8 +11,85 @@ module.exports = (middlewares) => {
         middlewares.forEach(middleware => router.use(middleware));
     }
 
+    /**
+     * @swagger
+     * /grupo/id/{id}:
+     *   get:
+     *     tags:
+     *       - Grupo
+     *     description: Retorna todas as informações do grupo de acordo com o seu id
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: id
+     *         description: id do grupo
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Dados do grupo
+     *       400:
+     *         description: Necessário enviar um id do grupo como parâmetro
+     *       500:
+     *         description: Erro inesperado
+     */
     router.get('/id/:id', controller.getGrupoById);
+
+    /**
+     * @swagger
+     * /grupo/nome/{nome}:
+     *   get:
+     *     tags:
+     *       - Grupo
+     *     description: Retorna todas as informações do grupo de acordo com o seu nome
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: nome
+     *         description: nome do grupo
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Dados do grupo
+     *       400:
+     *         description: Necessário enviar um nome do grupo como parâmetro
+     *       500:
+     *         description: Erro inesperado
+     */
     router.get('/nome/:nome', controller.getGrupoByNome);
+
+    /**
+     * @swagger
+     * /grupo:
+     *   post:
+     *     tags:
+     *       - Grupo
+     *     description: Cadastra um grupo
+     *     consumes:
+     *       - application/json
+     *     parameters:
+     *       - in: body
+     *         name: JSON contendo o nome do grupo
+     *         required: true
+     *         schema:
+     *           type: object
+     *           required:
+     *             - nome
+     *           properties:
+     *             nome:
+     *               type: string
+     *               example: Grupo 123
+     *     responses:
+     *       200:
+     *          description: Grupo criado
+     *       400:
+     *          description: Necessário enviar todos os dados obrigatórios
+     *       500:
+     *          description: Erro inesperado
+     */   
     router.post('/', controller.criarGrupo);
 
     return router;
--- server/routes/login/index.js
+++ server/routes/login/index.js
@@ -19,7 +19,75 @@ module.exports = (middlewares) => {
     }
 
     //router.get('/', redirectHome, controller.getLogin);
+
+    /**
+     * @swagger
+     * /login/aluno:
+     *   post:
+     *     tags:
+     *       - Login
+     *     description: Realiza uma operação de login de aluno
+     *     consumes:
+     *       - application/json
+     *     parameters:
+     *       - in: body
+     *         name: JSON contendo RA e senha
+     *         required: true
+     *         schema:
+     *           type: object
+     *           required:
+     *             - ra
+     *             - senha
+     *           properties:
+     *             ra:
+     *               type: integer
+     *               example: 123456
+     *             senha:
+     *               type: string
+     *               example: Joao123
+     *     responses:
+     *       200:
+     *          description: Tentativa de login realizado com sucesso
+     *       400:
+     *          description: Necessário enviar todos os dados obrigatórios
+     *       500:
+     *          description: Erro inesperado
+     */    
     router.post('/aluno', controller.loginAluno);
+
+    /**
+     * @swagger
+     * /login/professor:
+     *   post:
+     *     tags:
+     *       - Login
+     *     description: Realiza uma operação de login de professor
+     *     consumes:
+     *       - application/json
+     *     parameters:
+     *       - in: body
+     *         name: JSON contendo Matricula e senha
+     *         required: true
+     *         schema:
+     *           type: object
+     *           required:
+     *             - matricula
+     *             - senha
+     *           properties:
+     *             matricula:
+     *               type: integer
+     *               example: 112233
+     *             senha:
+     *               type: string
+     *               example: Carlos123
+     *     responses:
+     *       200:
+     *          description: Tentativa de login realizado com sucesso
+     *       400:
+     *          description: Necessário enviar todos os dados obrigatórios
+     *       500:
+     *          description: Erro inesperado
+     */    
     router.post('/professor', controller.loginProfessor);
 
     return router;
