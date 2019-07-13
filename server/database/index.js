ffee2568568cf1864b810cfb986cdf5d1d5bdbdf
VitorArtoni <vitor_artonio@hotmail.com>
2019-07-13 11:57:05 -0300

    BD pegando credenciais do .env

--- server/database/index.js
+++ server/database/index.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 const util = require('util');
 
 let pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'sistema_360'
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    port: process.env.dbPort,
+    database: process.env.dbName
 });
 
 pool.getConnection((err, connection) => {
