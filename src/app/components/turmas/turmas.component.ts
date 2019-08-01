d54df6fbdcb065e50cbe0ba0d09558c061868b33
Vitor-Artoni-de-Marcio <vitor.artoni1@ibm.com>
2019-07-31 21:19:21 -0300

    Cadastro de Turmas

--- src/app/components/turmas/turmas.component.ts
+++ src/app/components/turmas/turmas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TurmasService } from '../../services/turmas.service';
 
 @Component({
   selector: 'app-turmas',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurmasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private turmasService: TurmasService) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  cadastrarTurma(event) {
+    event.preventDefault();
+    const target = event.target;
+    const idTurma = target.querySelector('#idTurma').value;
+    const disciplina = target.querySelector('#disciplina').value;
+    const semestre = target.querySelector('#semestre').value;
+    const ano = target.querySelector('#ano').value;
+
+    this.turmasService.cadastrarTurma(
+      {
+        idTurma,
+        disciplina,
+        semestre,
+        ano
+      }
+    )
+      .subscribe(success => {
+        console.log(success);
+      });
   }
 
 }
--- src/app/services/turmas.service.ts
+++ src/app/services/turmas.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HttpErrorHandler, HandleError } from '../services/http-error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TurmasService {
-  
-  constructor(private http: HttpClient) { }
+  private handleError: HandleError;
+
+  constructor(private http: HttpClient, httpErrorHandler: HttpErrorHandler) { 
+    this.handleError = httpErrorHandler.createHandleError('TurmasService');
+  }
 
   getTurmas(){
     return this.http.get<any>('/turmas');
+  }
+
+  cadastrarTurma(turma: { idTurma: string, disciplina: string, semestre: string, ano: string}): Observable<boolean> {
+    return this.http.post<any>('/turmas', turma)
+      .pipe(
+        catchError(this.handleError('cadastrarAluno',turma))
+      );
   }
 }
