d500632b30bd4bac92639f67557543a36871f079
VitorArtoni <vitor_artonio@hotmail.com>
2019-08-10 15:29:27 -0300

    Script SQL Atualizado

--- sistema_360.sql
+++ sistema_360.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Jul-2019 às 00:57
+-- Generation Time: 10-Ago-2019 às 18:26
 -- Versão do servidor: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -120,6 +120,17 @@ CREATE TABLE `aluno` (
   `Senha` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`RA`, `Nome`, `Senha`) VALUES
+(111111, 'teste', '$2a$12$vI7mUHVzXyszxSQXlk9/iuwYr9AI0xo/WdAYbxHP.Q9mTaYmFbJRG'),
+(123456, 'João', '$2a$12$uCa1P9VyxaR7LaS0Gw8znenXoXxl2k5S0macgODtUy.49j407C.ya'),
+(174909, 'Mirelle', '$2a$12$kQyXpJbnxzjs2/nOHRUCW.bpjW4kAyQg1/xQn6trNWJAtapsGHgmC'),
+(176066, 'Rafael', '$2a$12$Ttny6rUt2Qr1.h5B2xTOwOHWWqOMycNeshb9MYhCDtlgrat2ontFG'),
+(178379, 'Vitor', '$2a$12$iU./p7G4OwhraAT80s/EXeNAYHsshZA9yAG1yHsiCp2xNVFyZMCP.');
+
 -- --------------------------------------------------------
 
 --
@@ -130,8 +141,21 @@ CREATE TABLE `aluno_presente_aula` (
   `RA` int(11) NOT NULL,
   `Turma` varchar(12) NOT NULL,
   `idAula` int(11) NOT NULL,
-  `Presenca` float NOT NULL
+  `Falta` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aluno_presente_aula`
+--
+
+INSERT INTO `aluno_presente_aula` (`RA`, `Turma`, `idAula`, `Falta`) VALUES
+(123456, 'SI250A', 4, 0.5),
+(123456, 'SI250A', 22, 0.5),
+(178379, 'SI250A', 18, 0.5),
+(178379, 'SI250A', 20, 0.2),
+(178379, 'SI250A', 21, 0),
+(178379, 'SI250A', 22, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -147,6 +171,16 @@ CREATE TABLE `aluno_realiza_atividade` (
   `Observacao` varchar(3000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `aluno_realiza_atividade`
+--
+
+INSERT INTO `aluno_realiza_atividade` (`Aluno_RA`, `idGrupo`, `idTopico`, `idTurma`, `Nota`, `Observacao`) VALUES
+(123456, 3, 'T1A1', 'SI250A', 0, NULL),
+(174909, 4, 'T1A1', 'SI250A', 4, NULL),
+(176066, 4, 'T1A1', 'SI250A', 5, NULL),
+(178379, 4, 'T1A1', 'SI250A', 2, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -157,9 +191,19 @@ CREATE TABLE `aluno_turma` (
   `idTurma` varchar(12) NOT NULL,
   `Aluno_RA` int(11) NOT NULL,
   `Nota` float NOT NULL DEFAULT '0',
-  `Status` set('Em Andamento','Aprovado','Reprovado','Exame') NOT NULL DEFAULT 'Em Andamento',
+  `Status` set('Em Andamento','Aprovado','Reprovado','Reprovado por Falta','Exame') NOT NULL DEFAULT 'Em Andamento',
   `idGrupo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aluno_turma`
+--
+
+INSERT INTO `aluno_turma` (`idTurma`, `Aluno_RA`, `Nota`, `Status`, `idGrupo`) VALUES
+('SI250A', 123456, 7.7931, 'Em Andamento', 3),
+('SI250A', 174909, 4.48707, 'Em Andamento', 4),
+('SI250A', 176066, 4.16983, 'Em Andamento', 4),
+('SI250A', 178379, 8.225, 'Em Andamento', 4);
 
 -- --------------------------------------------------------
 
@@ -179,6 +223,15 @@ CREATE TABLE `atividade_grupo` (
   `Nota_Grupo` float DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `atividade_grupo`
+--
+
+INSERT INTO `atividade_grupo` (`idGrupo`, `idTopico`, `idTurma`, `Observacao`, `Conformidade`, `Interesse`, `Contextualizacao`, `Consistencia`, `Nota_Grupo`) VALUES
+(1, 'T1A1', 'SI250A', '', 8, 6, 9.5, 6, 7.375),
+(3, 'T1A1', 'SI250A', '', 10, 10, 10, 10, 10),
+(4, 'T1A1', 'SI250A', '', 10, 7.5, 9.5, 9.5, 9.125);
+
 -- --------------------------------------------------------
 
 --
@@ -190,6 +243,24 @@ CREATE TABLE `aula` (
   `idTurma` varchar(12) NOT NULL,
   `Data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aula`
+--
+
+INSERT INTO `aula` (`idAula`, `idTurma`, `Data`) VALUES
+(4, 'SI250A', '2019-04-01'),
+(5, 'SI250A', '2019-04-02'),
+(6, 'SI250A', '2019-04-03'),
+(18, 'SI300A', '2019-08-01'),
+(19, 'SI300A', '2019-08-07'),
+(20, 'SI300A', '2019-08-14'),
+(21, 'SI300A', '2019-08-21'),
+(22, 'SI300A', '2019-08-30'),
+(23, 'SI300A', '2019-09-07'),
+(24, 'SI300A', '2019-09-14'),
+(25, 'SI300A', '2019-09-21'),
+(26, 'SI300A', '2019-09-30');
 
 -- --------------------------------------------------------
 
@@ -204,6 +275,18 @@ CREATE TABLE `contribuicao` (
   `Nota` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `contribuicao`
+--
+
+INSERT INTO `contribuicao` (`idTopico`, `RA`, `idTurma`, `Nota`) VALUES
+('T1A1', 123456, 'SI250A', 7.5),
+('T1A1', 174909, 'SI250A', 9),
+('T1A1', 176066, 'SI250A', 6.5),
+('T1A1', 178379, 'SI250A', 5.5),
+('T1A2', 123456, 'SI250A', 6.5),
+('T1A2', 178379, 'SI250A', 9);
+
 -- --------------------------------------------------------
 
 --
@@ -214,6 +297,16 @@ CREATE TABLE `grupo` (
   `idGrupo` int(11) NOT NULL,
   `NomeGrupo` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `grupo`
+--
+
+INSERT INTO `grupo` (`idGrupo`, `NomeGrupo`) VALUES
+(1, 'Grupo C'),
+(2, 'Grupo D'),
+(3, 'Grupo A'),
+(4, 'Grupo B');
 
 -- --------------------------------------------------------
 
@@ -226,6 +319,14 @@ CREATE TABLE `parametro` (
   `Chave` varchar(20) NOT NULL,
   `Valor` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `parametro`
+--
+
+INSERT INTO `parametro` (`Nome`, `Chave`, `Valor`) VALUES
+('peso_nota', 'apresentacao', '0.4'),
+('peso_nota', 'interacao', '0.6');
 
 -- --------------------------------------------------------
 
@@ -240,6 +341,16 @@ CREATE TABLE `participacao` (
   `Nota` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `participacao`
+--
+
+INSERT INTO `participacao` (`RA`, `idTurma`, `Eficiencia`, `Nota`) VALUES
+(123456, 'SI250A', 3.11111, 9.65517),
+(174909, 'SI250A', 1, 3.10345),
+(176066, 'SI250A', 0.722222, 2.24138),
+(178379, 'SI250A', 3.22222, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -252,6 +363,14 @@ CREATE TABLE `professor` (
   `Senha` varchar(100) NOT NULL,
   `Permissao` set('Docente','PED','PAD') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `professor`
+--
+
+INSERT INTO `professor` (`Matricula`, `Nome`, `Senha`, `Permissao`) VALUES
+(111111, 'Marli', '$2a$12$p.QPYjk3f6av0DaWI27BuOBenrAFMuEN4ZW7UBhmwgGtP7dXjLMiu', 'PAD'),
+(112233, 'Zambon', '$2a$12$uAcFqpbfsrb.cuziqyO3S.lS3DlfW4DzPqltE/1PGlBW8ZFyymkWK', 'Docente');
 
 -- --------------------------------------------------------
 
@@ -278,6 +397,15 @@ CREATE TABLE `topico` (
   `Data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `topico`
+--
+
+INSERT INTO `topico` (`idTopico`, `nomeTopico`, `objetivo`, `idTurma`, `Data`) VALUES
+('T1A1', 'Escolas de adm', 'Entender como funciona uma unidade sindical', 'SI250A', '2019-02-02'),
+('T1A2', 'Escolas de adm', 'Identificar qual a relação entre a produção e a padronização em uma manufatura', 'SI250A', '2019-02-03'),
+('T1A3', 'Escolas de adm', 'Entender como as tarefas são determinadas visando racionalizar o trabalho', 'SI250A', '2019-02-04');
+
 -- --------------------------------------------------------
 
 --
@@ -291,6 +419,15 @@ CREATE TABLE `turma` (
   `Ano` int(11) NOT NULL,
   `HorasAula` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `turma`
+--
+
+INSERT INTO `turma` (`idTurma`, `Disciplina`, `Semestre`, `Ano`, `HorasAula`) VALUES
+('SI250A', 'Economia e Finanças', 'S1', 2019, 2),
+('SI250B', 'Economia e Finanças', 'S1', 2019, 2),
+('SI300A', 'Programação', 'S2', 2020, 4);
 
 --
 -- Indexes for dumped tables
@@ -402,7 +539,7 @@ ALTER TABLE `turma`
 -- AUTO_INCREMENT for table `aula`
 --
 ALTER TABLE `aula`
-  MODIFY `idAula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idAula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `grupo`
 --
